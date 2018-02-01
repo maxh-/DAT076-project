d082294c7febbe5cd4f95120c47b7b5e7ef21f41
Max Hansson <maxh88@gmail.com>
2018-02-01 12:49:34 +0100

    make client fetch some test data from server

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  // get some data from server
+  componentDidMount() {
+    return fetch('/welcome')
+      .then(res => res.json())
+      .then(strings => this.setState({ strings }));
+  }
+
+  // render component
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.strings}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
