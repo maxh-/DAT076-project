1e5a8035eb94f39c01809d126a7eccd40bde860b
David Berg Marklund <davemcgroin@gmail.com>
2018-02-16 16:32:47 +0100

    moved some logic from route to controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const passport = require('passport');
+const isAuthenticated = require('../middlewares/isAuthenticated');
+const authController = require('../controllers/authController');
 
 /* GET users. */
-router.get('/', (req, res, next) => {
-  if(req.user){
-    res.send("you are logged in as " +req.user.email);
-  }else{
-    res.send(403).send("you are not logged in");
-  }
+router.get('/', isAuthenticated, async (req, res, next) => {
+  const response = await authController.findUserById(req.user.id);
+  res.json(response);
 });
 
 /* POST login */
@@ -21,26 +20,17 @@ router.post('/login',
 /* GET logout */
 router.get('/logout', (req, res, next) => {
   req.logout();
-  res.redirect('/auth');
+  res.json({
+    success: true,
+    code: 200,
+    message: "logout"
+  });
 });
 
 /* POST register user */
-router.post('/register', (req, res, next) => {
-  if(req.body.password === req.body.password2){
-    models.User.findOrCreate({where: {email: req.body.email},
-                              defaults:{ password: req.body.password,
-                                         firstName: req.body.firstName,
-                                         lastName: req.body.lastName
-                                       }}).spread((user, created) => {
-      if(created){
-        res.send(user);
-      }else{
-        res.status(400).send("User already exists");
-      }
-    });
-  }else{
-    res.status(400).send("passwords dont match");
-  }
+router.post('/register', async (req, res, next) => {
+  const response = await authController.register(req.body);
+  res.json(response);
 });
 
 
