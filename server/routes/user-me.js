747a8f6cd68c44e8ec84ca5048040f68ffb241ea
David Berg Marklund <davemcgroin@gmail.com>
2018-02-20 14:35:27 +0100

    fixed stupid bug

--- server/routes/user-me.js
+++ server/routes/user-me.js
@@ -4,13 +4,13 @@ const userController = require('../controllers/userController');
 
 /* GET self */
 router.get('/', async (req, res, next) => {
-  const response = userController.findMe(req.user.id);
+  const response = await userController.findMe(req.user.id);
   res.json(response);
 });
 
 /* POST change password */
 router.post('/changePassword', async (req, res, next) => {
-  const response = user.controller.updatePassword(req.body);
+  const response = await userController.updatePassword(req.body);
   res.json(response);
 });
 
