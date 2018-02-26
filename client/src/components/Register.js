8b976c37b99c05ef46ba288f65f52f02a112cfc6
Max Hansson <maxh88@gmail.com>
2018-02-26 17:34:04 +0100

    refactor register form, add email validation

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,24 +1,79 @@
 import React, { Component } from 'react';
-import Form from './Form';
-import './css/Form.css';
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock,
+  Row,
+  Col
+} from 'react-bootstrap';
+import * as EmailValidator from 'email-validator';
+import './css/Register.css';
 
-class Register extends Component {
-
-state = {
-  fields:{}
-};
-
-onSubmit = fields => {
-  console.log(fields);
-}
-  // render component
+export default class Register extends Component {
+  onSubmit(fields) {
+    console.log(fields);
+  }
+  
   render() {
     return (
-      <div className="Register">
-      <h1>Registrera dig!</h1>
-      <Form onSubmit={fields => this.onSubmit(fields)}/>
+      <div className="register">
+        <h2>Registrering</h2>
+        <hr />
+        <RegisterForm />
       </div>
     );
   }
 }
-export default Register
+
+class RegisterForm extends Component {
+    constructor(props, context) {
+    super(props, context);
+
+    //this.handleEm = this.handleChange.bind(this);
+
+    this.state = {
+      email: ''
+    };
+  }
+
+  getValidEmailState() {
+    if (this.state.email.length > 0) {
+      const isValid = EmailValidator.validate(this.state.email);
+      if (isValid) return 'success';
+      else return 'error';
+    } else {
+      return null;
+    }
+  }
+
+  onEmailChange(e) {
+    this.setState({ email: e.target.value });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col md={7}>
+          <form>
+            <FormGroup
+              controlId="email"
+              validationState={this.getValidEmailState()}
+              bsSize="large">
+              <ControlLabel>E-post:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="exempel@email.com"
+                onChange={this.onEmailChange.bind(this)} />
+              <FormControl.Feedback />
+              <HelpBlock>help text</HelpBlock>
+            </FormGroup>
+          </form>
+        </Col>
+      </Row>
+    );
+  }
+}
+
