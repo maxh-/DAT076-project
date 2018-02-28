c6333a2ae2788bbbd46a7203d13be19ee62fa233
David Berg Marklund <davemcgroin@gmail.com>
2018-02-28 15:11:06 +0100

    changed authentication to use tokens

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,14 +1,22 @@
+const passport = require("passport");
+const cfg = require("../config/jwtConfig");
 // check if the user is authenticated
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()){
-    next();
-  } else {
-    res.json({
-      success: false,
-      code: 401,
-      message: "user not logged in"
-    });
-  }
-};
+const isAuthenticated = (req, res, next)=> {
+    passport.authenticate("jwt", cfg.jwtSession, (err, user, info) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({
+          success: false,
+          code: 401,
+          message: 'Unathoraized user'
+        });
+      }
+      req.user = user;
+      next();
+    })(req, res, next);
+}
 
 module.exports = isAuthenticated;
