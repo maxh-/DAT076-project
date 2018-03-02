5b1304d5776d564b910b3aba6d2311df2c10ebb7
linilss <linunil@student.chalmers.se>
2018-03-02 15:50:09 +0100

    almost done, need likes from backend

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Jumbotron, Grid, Row, Col,
-  Glyphicon, Button, Modal, Popover, 
-  OverlayTrigger, Carousel, Label } from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col, Glyphicon, Button,
+  Modal, Carousel, Label } from 'react-bootstrap';
 import './css/Recipe.css';
 
 class Recipe extends Component {
@@ -12,18 +11,17 @@ class Recipe extends Component {
       upVotes: "1",
       downVotes: "5421",
       time: "",
-      description:"Spaghetti carbonara – en älskad favorit med rökt fläsk eller bacon och grädde! Lika bra till släktmiddagen som till fredagsmyset.",
+      description:"",
       ingredients: [],
       steps: [],
       show: false,
-      tags: []
+      tags: [],
+      exists: false
     }
   }
 
   componentDidMount() {
-    // from the path `/inbox/messages/:id`
     let id = this.props.match.params.id;
-    var recipe;
     fetch('/recipe/'+id, {
       method: 'GET',
     }).then(res => res.json())
@@ -34,24 +32,34 @@ class Recipe extends Component {
         time: res.recipe.timeToComplete,
         steps: res.recipe.Steps,
         ingredients: res.recipe.RecipeIngredients,
-        tags: res.recipe.Tags
+        tags: res.recipe.Tags,
+        exists: true
       });
+    })
+    .catch(error => { 
+      console.log(error);
+      this.setState({
+        title:"404: Receptet kunde inte hittas",
+        description: error,
+        exists: false
+      })
+
     });
   }
 
-  cookingMode = (e) => {
-    console.log("cooking mode activated!!!");
+  cookingMode(e){
     this.setState({ show: true });
   }
-  closeCookingMode = (e) => {
+  closeCookingMode(e){
     this.setState({show:false});
   }
-  displayModalSteps = (e) => {
+  displayModalSteps(e){
     let stps = [];
     this.state.steps.forEach(function(stp) {
       stps.push(
         <Carousel.Item key={stp.number} className="carouselItem">
           <h1>{stp.number}</h1>
+          <hr/>
           <b>
             {stp.instruction}
           </b>
@@ -60,88 +68,115 @@ class Recipe extends Component {
     });
     return stps;
   }
-  tags() {
+
+  showJumbotron() {
+    if(this.state.exists) {
+      return  (
+        <div>
+          <p>
+            <Glyphicon glyph=" glyphicon glyphicon-thumbs-up "  />
+            <small>({ this.state.upVotes })</small>
+            <Glyphicon glyph=" glyphicon glyphicon-thumbs-down "  />
+            <small>({ this.state.downVotes })</small>
+            <Glyphicon glyph=" glyphicon glyphicon glyphicon-time " id="glyph-space" />
+            <small> {this.state.time} minuter </small>
+
+          </p>
+          <p>
+           { this.state.description }
+          </p>
+        </div>
+      );
+    }
+  }
+
+  showTags() {
     let tgs = [];
     this.state.tags.forEach(function(tag) { 
       tgs.push(
-          <Label className="tag-label" >
-            <b># </b>{ tag.tag }
-          </Label>
+        <Label className="tag-label" >
+          <b># </b>{ tag.tag }
+        </Label>
       );
     });
     return tgs;
   }
-
-
-  render() {
-    let ingrs = [];
-    this.state.ingredients.forEach(function(ingr) {
-      ingrs.push(
-          <li key={ingr.number}> 
-            <b>{ ingr.Ingredient.name } </b>
-            <small> { ingr.amount } { ingr.Unit.name }</small>
-          </li>
-        );
-    });
+  showSteps() {
     let stps = [];
-    this.state.steps.forEach(function(stp) {
-      stps.push(
+    if(this.state.exists) {
+      stps.push(  
+        <h1> 
+          Instruktioner 
+          <Button bsStyle="success" className="pad" 
+              onClick={ this.cookingMode.bind(this) }>
+            Börja laga  
+          </Button>
+        </h1>
+      );
+      this.state.steps.forEach(function(stp) {
+        stps.push(
           <li key={stp.number} className="itemInList"> 
             <p>
               { stp.instruction } 
             </p>
           </li>
-        );
-    });
+        );    
+      });
+    }
+    return stps;
+  }
 
+  showIngredients() {
+    let ingrs = [];
+    if(this.state.exists) {
+      ingrs.push(  
+        <h1> Ingredienser </h1>
+      );
+      this.state.ingredients.forEach(function(ingr) {
+        ingrs.push(
+          <li key={ingr.number}> 
+            <b>{ ingr.Ingredient.name } </b>
+            <small> { ingr.amount } { ingr.Unit.name }</small>
+          </li>
+        );
+      });
+    }
+    return ingrs;
+  }
+
+
+  render() {
     return (
       <div id="mainContainer">
-        <div>
-        <img src="/img/bild.jpg" id="pic" alt="bild"/>
+        <div id="title-div">
+          <img src="/img/bild.jpg" id="pic" alt="bild"/>
           <Jumbotron>
             <h1> { this.state.title } </h1>
-            <p>
-              <Glyphicon glyph=" glyphicon glyphicon-thumbs-up "  />
-              <small>({ this.state.upVotes })</small>
-              <Glyphicon glyph=" glyphicon glyphicon-thumbs-down "  />
-              <small>({ this.state.downVotes })</small>
-              <Glyphicon glyph=" glyphicon glyphicon glyphicon-time " id="glyph-space" />
-              <small> {this.state.time} timmar </small>
-
-
-            </p>
-            <p>
-             { this.state.description }
-            </p>
-            <p>
-              { this.tags() }
-            </p>
+            { this.showJumbotron() }
+            <div id="show-tags">
+              { this.showTags() }
+            </div>
           </Jumbotron>
-        </div>
+        </div>;
+
         <hr/>
+
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={4} id="ingrs"  className="lists">
-              <h1> Ingredienser </h1>
-              <ul>
-                { ingrs }
+              <ul id="ingredients-list">
+                { this.showIngredients() }
               </ul>
             </Col>
             <Col xs={12} md={8} id="steps" className="lists">
-              <h1> 
-                Instruktioner 
-                  <Button bsStyle="success" className="pad" 
-                      onClick={ this.cookingMode.bind(this) }>
-                    Börja laga  
-                  </Button>
-              </h1>
-              <ol>
-                { stps }  
+              <ol id="steps-list">
+                { this.showSteps() }
               </ol>
             </Col>
           </Row>
         </Grid>
-         <Modal id="modal" show={this.state.show} onHide={this.closeCookingMode.bind(this)}>
+
+        <Modal id="modal" show={this.state.show} onHide={this.closeCookingMode.bind(this)}>
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
