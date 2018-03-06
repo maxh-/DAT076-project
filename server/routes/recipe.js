ccd47f07781b60ea196aa7ecc235fa645ebf5ab3
David Berg Marklund <davemcgroin@gmail.com>
2018-03-06 16:47:53 +0100

    added favorites to model and routes

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -33,6 +33,18 @@ router.delete("/:id/like", isAuthenticated, async (req, res, next) => {
   res.status(response.code).json(response);
 });
 
+/* POST favorite a recipe */ 
+router.post("/:id/favorite", isAuthenticated, async (req, res, next) => {
+  const response = await recipeController.favorite(req.params.id, req.user.id);
+  res.status(response.code).json(response);
+});
+
+/* POST remove favorite */ 
+router.delete("/:id/favorite", isAuthenticated, async (req, res, next) => {
+  const response = await recipeController.removeFavorite(req.params.id, req.user.id);
+  res.status(response.code).json(response);
+});
+
 /* GET Recipe by id . */
 router.get('/:id', async (req, res, next) => {
   const response = await recipeController.findById(req.params.id);
