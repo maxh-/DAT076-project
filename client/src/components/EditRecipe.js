60c77c573c423f8ec0cf8964e19ada1bcd00353e
Max Hansson <maxh88@gmail.com>
2018-03-08 19:35:45 +0100

    wip: edit-recipe page

---
+++ client/src/components/EditRecipe.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import EditRecipeStore from '../util/editRecipeStore';
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+  Form,
+  Col,
+  Button
+} from 'react-bootstrap';
+import './css/EditRecipe.css';
+import Auth from '../util/AuthService';
+
+const EditRecipe = observer(class EditRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.store = new EditRecipeStore(this.props.match.params.id);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeMealType = this.handleChangeMealType.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.store.update();
+  }
+
+  render() {
+    return this.store.recipe.UserId === Auth.user.id
+      ? this.RenderRecipePage()
+      : <div></div>;
+  }
+
+  RenderRecipePage(props) {
+    return (
+      <div className="edit-recipe">
+        <h2>Redigera recept</h2>
+        <hr />
+        <form action={this.handleSubmit}>
+
+          {/**  Titel **/}
+
+          <FieldGroup
+            id="title"
+            name="title"
+            type="text"
+            label="Titel"
+            onChange={this.handleChange}
+            value={this.store.recipe.title}
+          />
+
+          {/**  Tid **/}
+
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Tidsåtgång</ControlLabel>
+            <FormControl componentClass="select"
+              placeholder="select"
+              value={0}
+              onChange={this.handleChange}
+              name="timeToComplete"
+              defaultValue={this.store.recipe.timeToComplete}>
+              <option value={0}>{ formatTime(this.store.recipe.timeToComplete) }</option>
+              <option value={15}>0:15</option>
+              <option value={30}>0:30</option>
+              <option value={45}>0:45</option>
+              <option value={60}>1:00</option>
+              <option value={90}>1:30</option>
+              <option value={120}>2:00</option>
+              <option value={240}>4:00</option>
+              <option value={480}>8:00</option>
+              <option value={1440}>24:00+</option>
+            </FormControl>
+          </FormGroup>
+
+          {/**  Måltid  **/}
+
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Måltid</ControlLabel>
+            <FormControl
+              componentClass="select"
+              placeholder="select"
+              value={this.store.mealType.id}
+              onChange={this.handleChangeMealType}
+              name="mealType"
+              >
+              {this.store.allMealTypes.map(tag => {
+                return <option value={tag.id}>{tag.tag}</option>;
+              })}
+            </FormControl>
+          </FormGroup>
+
+          {/**  Beskrivning  **/}
+
+          <FieldGroup
+            id="tweet"
+            name="tweet"
+            type="text"
+            label="Beskrivning"
+            onChange={this.handleChange}
+            value={this.store.recipe.tweet}
+          />
+
+          {/**  Taggar  **/}
+
+          {this.store.allTags.map(tag => {
+            return <TagButton store={this.store} tag={tag} />
+          })}
+
+
+        </form>
+        <pre>{ JSON.stringify(this.store.recipe, null, 2) }</pre>
+      </div>
+    );
+  }
+
+  handleChange({ target }) {
+    this.store.recipe[target.name] = target.value;
+  }
+
+  handleChangeMealType({ target }) {
+    this.store.setMealType(target.value);
+  }
+
+  handleSubmit() {
+
+  }
+})
+
+const FieldGroup = ({ id, label, help, ...props }) => {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+};
+
+const formatTime = (minutes) => {
+  let hours = Math.floor(minutes / 60);
+  let mins = minutes % 60;
+  hours = hours < 10 ? '0' + hours : hours;
+  mins = mins < 10 ? '0' + mins : mins;
+  return `${hours}:${mins}`;
+}
+
+class TagButton extends Component {
+  constructor(props) {
+    super(props);
+    this.store = props.store;
+    this.state = {
+      checked: props.tag.checked,
+      id: props.tag.id,
+      name: props.tag.tag
+    };
+  }
+
+  render() {
+    return (<Button className={this.state.checked ? "active" : ""}>{this.state.name}</Button>);
+  }
+}
+
+export default EditRecipe;
---
+++ client/src/util/editRecipeStore.js
@@ -0,0 +1,59 @@
+import { extendObservable, action } from 'mobx';
+import { get } from 'axios';
+import Auth from './AuthService'
+
+class EditRecipeStore {
+  constructor(recipeId) {
+    extendObservable(this, {
+      recipe: {},
+      id: recipeId,
+      allTags: [], 
+      allMealTypes: [],
+      mealType: {}, // mealType tag
+      otherTags: [], // "regular" tags
+      update: action(async () => {
+        await get(`/recipe/${this.id}`).then(res => {
+          this.recipe = res.data.recipe;
+          this.mealType = getMealType(res.data.recipe.Tags);
+        });
+        await get(`/tag`).then(res => {
+          this.allTags = res.data.tags;
+          this.allMealTypes = filterTags(res.data.tags, [1, 2, 3, 4]);
+        });
+        this.otherTags = this.recipe.Tags.filter(tag => {
+          return !this.allMealTypes.includes(tag);
+        });
+        this.allTags.map(tag => {
+          // add 'checked' boolean prop to tags in allTags
+          this.otherTags.find(otherTag => otherTag.id === tag.id)
+            ? tag.checked = true
+            : tag.checked = false;
+        });
+      }),
+      setMealType: (id) => {
+        const oldMealType = this.mealType;
+        const newMealType = this.allMealTypes.find(tag => {
+          return tag.id === parseInt(id);
+        });
+        this.mealType = newMealType;
+        const replaceIndex = this.recipe.Tags.findIndex(tag => {
+          return tag.id === oldMealType.id;
+        })
+        this.recipe.Tags[replaceIndex] = newMealType;
+      }
+    })
+  }
+}
+
+const filterTags = (tags, tagIDs) => {
+  return tags.filter(tag => tagIDs.includes(tag.id));
+}
+
+const getMealType = (tags) => {
+  const mealTypeTagIDs = [1, 2, 3, 4];
+  return tags.find(tag => mealTypeTagIDs.includes(tag.id));
+}
+
+export default EditRecipeStore;
+
+window.ers = new EditRecipeStore(2);
