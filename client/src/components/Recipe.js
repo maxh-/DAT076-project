2200e824af15a7d7bcbf98705d813e3db94a2c15
linilss <linunil@student.chalmers.se>
2018-03-08 18:41:26 +0100

    removed some stuff

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -293,20 +293,3 @@ const RecipeImage = observer(class RecipeImage extends Component {
 });
 
 export default Recipe;
-
-class LikeButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      liked: false
-    }
-  }
-
-  render() {
-    return(
-      <Button>
-
-      </Button>
-    );
-  }
-}
