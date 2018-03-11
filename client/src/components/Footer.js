12eac295077579bdd98d2fcdd938fb74b93043f8
Max Hansson <maxh88@gmail.com>
2018-03-11 15:47:09 +0100

    style footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import './css/Footer.css';
 
 class Footer extends Component {
   render() {
     return(
-      <Navbar className="Footer" inverse collapseOnSelect>
-          <h3>Footer</h3>
-      </Navbar>
+      <div className="footer">
+        <Row>
+          <Col md={1} mdOffset={1} className="footer-brand">
+            Receptsidan<br />
+            <small>a DAT-076 project</small>
+          </Col>
+          <Col md={10} className="footer-masthead">
+            Adis Mahmutovic<br />
+            David Berg Marklund<br />
+            Linus Nilsson<br />
+            Max Hansson
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
