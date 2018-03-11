c0ff7e7c09746c6c1bcd9acd37c5196b3ac4b4c9
linilss <linunil@student.chalmers.se>
2018-03-11 15:20:08 +0100

    added pullRight to rightmost dropdown so it won't exceed max screen width

--- client/src/components/Browse.js
+++ client/src/components/Browse.js
@@ -196,8 +196,9 @@ const Browse = observer(class Browse extends Component {
               </DropdownButton>
             </Col>
             <Col xs={3}>
-              <DropdownButton title="Specialkost" className="btns" id="d">
+              <DropdownButton title="Specialkost" className="btns" pullRight>
                 <ToggleButtonGroup type="radio" name="filter"
+											id="d"
                       value={this.state.filter}
                       onClick={this.addFilter.bind(this)}>
       			      <ToggleButton className="dropdownItem" value={8}>lakto-vegetarianskt</ToggleButton>
