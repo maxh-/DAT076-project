ffa3781c8b8d09087b4c36c4b47319c34f462810
Max Hansson <maxh88@gmail.com>
2018-03-11 15:52:11 +0100

    footer spelling fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
         <Row>
           <Col md={1} mdOffset={1} className="footer-brand">
             Receptsidan<br />
-            <small>a DAT076 project</small>
+            <small>DAT076 projekt</small>
           </Col>
           <Col md={10} className="footer-masthead">
             Adis Mahmutovic<br />
